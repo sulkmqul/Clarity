fef792173f7d8620f71b5df6630902831f8eedff
popo <gfgf>
2023-02-08 15:15:24 +0900

    モーション管理GUIの作成開始

---
+++ src/ClarityMovement/ClarityMovement/MainForm.cs
@@ -0,0 +1,28 @@
+namespace ClarityMovement
+{
+    /// <summary>
+    /// ÉÅÉCÉìâÊñ 
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        //--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//
+        //--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//
+        //--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//
+        /// <summary>
+        /// ì«Ç›çûÇ‹ÇÍÇΩéû
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
